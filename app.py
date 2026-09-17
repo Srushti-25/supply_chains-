848dbd98d9244edeb29454f19b1ab2425ec2be9a
Srushti Rajesh Daware <srushtidaware143@gmail.com>
2026-09-17 12:24:15 +0530

    Uncomment model loading in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-# model = joblib.load('iris_model.pkl')
+model = joblib.load('iris_model.pkl')
 
 # Set the page title
 st.title("Machine Learning on Iris Data")
